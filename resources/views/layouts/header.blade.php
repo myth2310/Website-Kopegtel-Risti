defc70384f30751ca7f66b69f32cf785ca906305
Rifai04 <isnan.fai@gmail.com>
2022-08-15 17:18:55 +0700

    Fixing breadcrumbs

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,22 +3,52 @@
     style="position: fixed; width: 100%; top: 0; z-index: 1; padding-left:262px"
 >
     <div class="container-fluid">
-        <div class="col">
-            <div 
-                class="breadcrumb breadcrumb-dark bg-primary"
-                style="margin: 0px; padding: 0px"
-            >
-                <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-                <li class="breadcrumb-item"><a href="#">Library</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Data</li>
-            </div>                         
-            <h6 
-                class="h2 text-white d-inline-block"
-                style="margin: 0px"
-            >
-                Default
-            </h6> 
-        </div>
+        
+        <ul style="margin: 0px; padding: 0px">
+            <div class="col">
+                <?php 
+                    $link = "";
+                    $prev = "";
+                    $now = "";
+                ?>
+                <ol 
+                    class="breadcrumb breadcrumb-dark bg-primary"
+                    style="margin: 0px; padding: 0px"
+                >            
+                    <li class="breadcrumb-item">
+                        <a href="/dashboard">Kopegtel Risti</a>
+                    </li>                        
+                    @for($i = 1; $i <= count(Request::segments()); $i++)
+                        @if($i < count(Request::segments()) && $i > 0)
+                            <?php $link = "/".Request::segment($i); ?>
+                            <li class="breadcrumb-item active"> {{ ucwords(str_replace('-',' ',Request::segment($i)))}} </li>
+                            <?php $now = ucwords(str_replace('-',' ',Request::segment($i))); ?>
+                            @if ($i == 1 )
+                                <?php $prev = ucwords(str_replace('-',' ',Request::segment($i))); ?>                                
+                            @endif
+                        @elseif ($i < 3)
+                            <li class="breadcrumb-item active"> {{ucwords(str_replace('-',' ',Request::segment($i)))}} </li>
+                            <?php $now = ucwords(str_replace('-',' ',Request::segment($i))); ?>
+                        @endif
+                    @endfor
+                </ol>
+                <h6 
+                    class="h2 text-white d-inline-block"
+                    style="margin: 0px"
+                >
+                    @if ($prev == "")
+                        {{$now}}
+                    @else       
+                        @if ($now =="Create")                            
+                            {{$now}} New {{$prev}}
+                        @else  
+                            {{$now}} {{$prev}}
+                        @endif                 
+                    @endif
+                </h6>             
+            </div>
+        </ul>
+
         <ul class="navbar-nav ml-lg-auto" style="align-items: center">
             <div style="height: 24px; border-left: solid 1px rgb(232, 232, 232)"></div>
             <li class="nav-item dropdown">
@@ -30,7 +60,7 @@
                         </span>
                         </div>
                     </div>
-                    </a>                    
+                </a>                    
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbar-default_dropdown_1">
                     <form action="/logout" method="POST">
                     @csrf
@@ -40,6 +70,7 @@
                     </form>
                 </div>
             </li>
-        </ul>            
+        </ul>
+
     </div>
 </nav>
